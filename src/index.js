7f3b37d628365b4ba2da767deb04e28542e040eb
Shlomi Yahbes <99732661+syahbes@users.noreply.github.com>
2022-07-26 18:02:25 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+//import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+
+import App from "./App";
+import store from "./app/store";
+import "antd/dist/antd.css";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>
+);
+
+// ReactDOM.render(
+//  <Router>
+//      <App />
+//  </Router>,
+//  document.getElementById('root')
+//  );
